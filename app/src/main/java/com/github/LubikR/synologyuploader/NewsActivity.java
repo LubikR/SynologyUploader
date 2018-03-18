4392cb9f043d095175183c31e59767748df43016
LubikR <renner.lubos@seznam.cz>
2018-03-18 15:15:32 +0100

    NewsActivity - Camera OK button also close the Activity

--- app/src/main/java/com/github/LubikR/synologyuploader/NewsActivity.java
+++ app/src/main/java/com/github/LubikR/synologyuploader/NewsActivity.java
@@ -2,7 +2,10 @@ package com.github.LubikR.synologyuploader;
 
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.widget.TextView;
+
+import com.sony.scalar.sysutil.ScalarInput;
 
 public class NewsActivity extends BaseActivity {
 
@@ -15,5 +18,15 @@ public class NewsActivity extends BaseActivity {
 
         news = (TextView) findViewById(R.id.textViewNews);
         SharedPreferencesManager.write(getString(R.string.versionReadTag), "1.4");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (event.getScanCode()) {
+            case ScalarInput.ISV_KEY_ENTER:
+                return onDeleteKeyUp();
+            default:
+                return super.onKeyDown(keyCode, event);
+        }
     }
 }
